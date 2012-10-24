d01ddd3ba289a01b08c02e993dd54b5163103a3a
Orangenpresse <orangenpresse@arcor.de>
2012-10-25 01:35:20 +0200

    moved to express.js, sessions implemented

--- server.js
+++ server.js
@@ -1,91 +1,56 @@
 #!/usr/local/bin/node
 
-var http = require('http');
+var express = require("express");
 var path = require('path');
 var fs = require('fs');
-var app = require('./user.js');
-var users = [];
+var app = express();
+
+var becher = require('./user.js');
+
+app.use(express.cookieParser());
+app.use(express.cookieSession({
+	key: "becherapp",
+	secret: new Date().getTime().toString()	
+}));
 
 
-http = http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-});
-
-http.on('request', function(request, response) {
-	var filePath = getFilePath(request);
+app.get( /.*/,function(req, res) {
+	var filePath = getFilePath(req);
 	
 	fs.exists(filePath, function(exists) {
 
 		if (exists) {
 			fs.readFile(filePath, function(error, content) {
 				if (error) {
-					response.writeHead(500);
-					response.end();
+					res.send(500);
 				} else {
-					app.parseParams(request, getUser(request));
-					response.writeHead(200, {
-						'Content-Type': 'text/html'
-					});
-					response.write("Stuck: " + count("Stuck")
-						       + "<br />Hard: " + count("Hard")
-						       + "<br />Good: " + count("Good")
-						       + "<br /><br />Users: " + users.length
-						       );
-					response.end(content, 'utf-8');
+					becher.parseParams(req);
+					res.set('Content-Type', 'text/html');
+					
+					res.send("Stuck: " + becher.count("stuck")
+						+ "<br />Hard: " + becher.count("hard")
+						+ "<br />Good: " + becher.count("good")
+						+ "<br /><br />Users: " + becher.count("users")
+						+ content
+						);
 
 				}
 			});
 		} else {
-			response.writeHead(404);
-			response.end();
+			res.send(404);
 		}
 		
 	});
-
-
-
 });
 
-
-function getFilePath(request) {
-	var filePath = '.' + request.url.replace(/\?.*/g,"");
+function getFilePath(req) {
+	var filePath = '.' + req.path;
 	if (filePath == './')
 		return './index.html';
 		
 	return filePath;
 }
 
+app.listen("8080");
 
-function count(status) {
-	var number = 0;
-	
-	for( var i = 0; i < users.length; i++) {
-		if(users[i].user != null && users[i].user.status.value == status)
-			number++;
-	}
-	
-	return number;
-}
-
-function getUser(request) {
-	//TODO session management
-	for( var i = 0; i < users.length; i++) {
-		if(users[i].socket == request.socket)
-			return users[i];
-	}
-	return null;
-}
-
-http.on('connection', function(socket) {
-	//TODO session management
-	users.push({
-		"socket": socket,
-		user: null,
-		time: new Date().getTime()
-	});
-});
-
-http.listen("8080");
 
--- user.js
+++ user.js
@@ -1,43 +1,130 @@
-function User() {
+/*
+ * holds users
+ */
+var users = {};
+
+/*
+ * 5 seconds in ms
+ */
+const fiveSeconds = 5000;
+
+/*
+ * User prototype
+ */
+function User(sessionId) {
+	
+	this.sessionId = sessionId;
 	this.status = new None();
+	this.lastActivity = new Date().getTime();
+	
+	this.setLastActivity = function() {
+		this.lastActivity = new Date().getTime();
+	}
+	
+	this.getLastActivity = function() {
+		return this.lastActivity
+	}
 	
 	this.setStatus = function(status) {
 		this.status = status;
 	}
 	
 	this.getStatus = function(status) {
-		return status.color;
+		return this.status;
 	}
 }
 
+/*
+ * Status protoypes
+ */
 function None() {
-	this.value= "None";
+	this.value= "none";
 }
 
 function Stuck() {
-	this.value= "Stuck";
+	this.value= "stuck";
 }
 
 function Hard() {
-	this.value= "Hard";
+	this.value= "hard";
 }
 
 function Good() {
-	this.value= "Good";
+	this.value= "good";
 }
 
-function parseParams(request, user) {
-	if(user.user != null)
-	    return setStatus(request, user.user);
+/*
+ * Checks if clients are active
+ */
+function checkUsers(activeTime) {
+	var timeNow = new Date().getTime();
+	
+	for( var user in users) {
+		if(users[user].getLastActivity() < timeNow - activeTime)
+			delete users[user];
+		
+	}
+}
 
-        user.user = new User();
-        return setStatus(request, user.user);
+/*
+ * Registers User session
+ */
+function registerUser(reg) {
+	var sessionId = new Date().getTime().toString() + Object.keys(users).length.toString();
+	reg.session = sessionId;
+	users[sessionId] = new User(sessionId);
+	
+	return users[sessionId];
+}
+
+/*
+ * Gets the User by request
+ */
+function getUser(req) {
+	if(users[req.session] != undefined) {
+		return users[req.session];
+	}
+	else {
+		return registerUser(req);
+	}
+}
+
+/*
+ * Counts users or status
+ */
+function count(value) {
+	if(value == "users")
+		return  Object.keys(users).length;
+	
+	var count = 0;
+	
+	for( var user in users) {
+		if(users[user].status.value == value)
+			count++;
+	}
+	
+	return count;
+}
+
+/*
+ * Parses the request params and starts the logic
+ */
+function parseParams(req) {
+	var user = getUser(req);
+	user.setLastActivity();
+	
+	checkUsers(fiveSeconds);
+	
+        return setStatus(req, user);
 };
 
+/*
+ * Sets the status by query.status
+ */
 function setStatus(request, user) {
 	query = require('url').parse(request.url, true).query;
 	
-	switch (query.light) {
+	switch (query.status) {
 		case "stuck":
 			user.setStatus(new Stuck());
 			break;
@@ -51,3 +138,4 @@ function setStatus(request, user) {
 }
 
 module.exports.parseParams = parseParams;
+module.exports.count = count;
